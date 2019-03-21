029f4c0a2e5c70f5de3e3369f4a0f965280d7419
Joy Lin <vvlnote@hotmail.com>
2019-03-21 20:36:21 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def breed=(d_breed)
+    @breed = d_breed
+  end
+  def breed 
+    @breed
+  end
+  
+  def name=(d_name)
+    @name = d_name
+  end
+  
+  def name 
+    @name
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def name=(p_name)
+    @name = p_name
+  end
+  def name
+    @name
+  end
+  
+  def job=(job_title)
+    @job = job_title
+  end
+  
+  def job
+    @job
+  end
+end
